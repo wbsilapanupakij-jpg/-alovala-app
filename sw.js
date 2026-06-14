463b62ba6bae641a133c297be5c1b1df36be040a
wbsilapanupakij-jpg <wb.silapanupakij@gmail.com>
2026-06-14 14:48:58 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() {
+    return new Response('Offline - กรุณาเชื่อมต่ออินเทอร์เน็ต', {
+      headers: { 'Content-Type': 'text/html; charset=UTF-8' }
+    });
+  }));
+});
